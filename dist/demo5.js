f261e8d07d55698937b759a20241c1cfd07ae664
chenjun <chenjun@orvibo.com>
2022-04-29 16:56:23 +0800

    tsc编译ts到js

--- dist/demo5.js
+++ dist/demo5.js
@@ -1,4 +1,5 @@
 "use strict";
+// 泛型
 const calcArray = (data) => {
     let list = [];
     for (let i = 0; i < 3; i++) {
@@ -9,3 +10,4 @@ const calcArray = (data) => {
 console.log(calcArray(1));
 console.log(calcArray("china"));
 console.log(calcArray({ name: "jack", age: 30 }));
+console.log(calcArray({ address: "广东深圳", phone: 13562563254 }));
---
+++ dist/demo7.js
@@ -0,0 +1,17 @@
+"use strict";
+// 泛型类
+class calcArray2 {
+    constructor() {
+        this.arr = [];
+    }
+    add(value) {
+        this.arr.push(value);
+    }
+    getValue() {
+        return this.arr;
+    }
+}
+const res = new calcArray2();
+res.add(1);
+res.add("1");
+console.log(res.getValue());
