988bf86ac2a127136ea8bf5da9a7cf5b264c9035
chenjun <chenjun@orvibo.com>
2023-01-12 17:54:00 +0800

    utility-types

--- dist/demo8.js
+++ dist/demo8.js
@@ -1,7 +1,26 @@
 "use strict";
 const map = { a: 1 };
-const person = {
-    name: 'jack'
-};
 console.log(map);
-console.log(person);
+const person1 = {
+    name: 'Jack'
+};
+console.log(person1);
+const person2 = {
+    name: 'Lucy',
+    age: 20,
+    address: 'China BeiJing'
+};
+console.log(person2);
+const person3 = {
+    name: 'Tony',
+    age: 33,
+    address: 'China GuangZhou'
+};
+// person3.age=25; 只读属性，不可赋值
+console.log(person3);
+const person4 = {
+    name: "James",
+    age: 20
+};
+console.log(person4);
+// 更多用法参考：https://www.typescriptlang.org/docs/handbook/utility-types.html#awaitedtype
--- src/demo8.ts
+++ src/demo8.ts
@@ -8,11 +8,47 @@ interface IUserInfo {
 // Record<string, unknown> 任意对象
 // {} 任何不为空的对象
 type obj = Record<string, unknown>;
-// 作用：Partial生成一个新类型，该类型与 T 拥有相同的属性，但是所有属性皆为可选项
-type man = Partial<IUserInfo>;
 const map: obj = { a: 1 };
-const person: man = {
-  name: 'jack'
-}
 console.log(map);
-console.log(person);
+
+// 作用：Partial生成一个新类型，该类型与 T 拥有相同的属性，但是所有属性皆为可选项
+type infoType1 = Partial<IUserInfo>;
+const person1: infoType1 = {
+  name: 'Jack'
+}
+console.log(person1);
+
+// 作用：生成一个新类型，该类型与 T 拥有相同的属性，但是所有属性皆为必选项
+type infoType2 = Required<IUserInfo>;
+const person2: infoType2 = {
+  name: 'Lucy',
+  age: 20,
+  address: 'China BeiJing'
+}
+console.log(person2);
+
+// 作用：生成一个新类型，T 中的 K 属性是只读的，K 属性是不可修改的。
+type infoType3 = Readonly<IUserInfo>;
+const person3: infoType3 = {
+  name: 'Tony',
+  age: 33,
+  address: 'China GuangZhou'
+}
+// person3.age=25; 只读属性，不可赋值
+console.log(person3);
+
+// 作用：生成一个新类型，该类型拥有 T 中的 K 属性集 ; 新类型 相当于 T 与 K 的交集
+type infoType4 = Pick<IUserInfo, 'name' | 'age'>;
+const person4: infoType4 = {
+  name: "James",
+  age: 20
+}
+console.log(person4);
+
+// 更多用法参考：https://www.typescriptlang.org/docs/handbook/utility-types.html#awaitedtype
+
+ 
+
+
+
+
